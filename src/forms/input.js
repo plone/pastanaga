1c67026769dfb06666b41a6d9a9ea19a87cb64fe
Timo Stollenwerk <stollenwerk@kitconcept.com>
2017-07-03 16:11:56 +0300

    Add simple InputText component.

---
+++ src/forms/input.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class InputText extends Component {
+  render() {
+    return (
+      <input type="text" />
+    );
+  }
+}
+
+export default InputText;
