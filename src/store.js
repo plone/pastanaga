838380694b28c0b71bd0331763d115c4d90fd8d4
Timo Stollenwerk <stollenwerk@kitconcept.com>
2017-07-05 12:56:38 +0300

    Add basic routing with react-router-redux (next).

--- src/store.js
+++ src/store.js
@@ -1,47 +1,35 @@
-import createLogger from 'redux-logger';
-import { createStore, compose, applyMiddleware } from 'redux';
-import { routerMiddleware } from 'react-router-redux';
-import thunk from 'redux-thunk';
+import { createStore, applyMiddleware, compose } from 'redux'
+import { routerMiddleware } from 'react-router-redux'
+import thunk from 'redux-thunk'
+import createHistory from 'history/createBrowserHistory'
+import rootReducer from './modules'
 
-import reducer from './reducers';
+export const history = createHistory()
 
-/**
- * AddToMiddleWare.
- * @function addToMiddleWare
- * @param {Array} data Middleware Array
- * @param {Object} logger Logger instance
- * @returns {Array} middleware.
- */
-const addToMiddleWare = (data, logger) => {
-  const devTools = typeof window === 'object' &&
-    typeof window.devToolsExtension !== 'undefined'
-    ? window.devToolsExtension()
-    : f => f;
-  const debugLogger = logger ? applyMiddleware(createLogger()) : f => f;
-  return [...data, devTools, debugLogger];
-};
+const initialState = {}
+const enhancers = []
+const middleware = [
+  thunk,
+  routerMiddleware(history)
+]
 
-/**
- * Configure store.
- * @function configureStore
- * @param {Object} initialState state object
- * @param {Object} history state object
- * @param {bool} logger showLogger
- * @param {Object} apiHelper ApiHelper
- * @returns {Object} Store.
- */
-export default function configureStore(
-  initialState,
-  history,
-  logger
-) {
-  const middlewares = addToMiddleWare(
-    [applyMiddleware(routerMiddleware(history), thunk)],
-    logger,
-  );
+if (process.env.NODE_ENV === 'development') {
+  const devToolsExtension = window.devToolsExtension
 
-  const createStoreWithMiddleware = compose(...middlewares)(createStore);
-  const store = createStoreWithMiddleware(reducer, initialState);
-
-  return store;
+  if (typeof devToolsExtension === 'function') {
+    enhancers.push(devToolsExtension())
+  }
 }
+
+const composedEnhancers = compose(
+  applyMiddleware(...middleware),
+  ...enhancers
+)
+
+const store = createStore(
+  rootReducer,
+  initialState,
+  composedEnhancers
+)
+
+export default store
