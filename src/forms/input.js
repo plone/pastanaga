e59112598736cc28cada9ba2b1c27f8df02e7783
Timo Stollenwerk <stollenwerk@kitconcept.com>
2017-07-05 12:04:31 +0300

    Fix html attr to jsx compliant attributes in input.js.

--- src/forms/input.js
+++ src/forms/input.js
@@ -3,10 +3,25 @@ import React from 'react';
 
 const InputText = () => {
   return (
-    <div class="form-field">
-      <label for="form-text-id01" aria-describedby="form-text-id01-help" class="form-label">Text label <span class="form-required">(required)</span></label>
-      <input id="form-text-id01" type="text" name="form-text-id01" required="required" placeholder="Placeholder" class="form-text"/>
-      <p id="form-text-id01-help" class="form-help">Complementary and clarification to the label</p>
+    <div className="form-field">
+      <label
+        htmlFor="form-text-id01"
+        aria-describedby="form-text-id01-help"
+        className="form-label">
+          Text label <span className="form-required">(required)</span>
+      </label>
+      <input
+        id="form-text-id01"
+        type="text"
+        name="form-text-id01"
+        required="required"
+        placeholder="Placeholder"
+        className="form-text" />
+      <p
+        id="form-text-id01-help"
+        className="form-help">
+        Complementary and clarification to the label
+      </p>
     </div>
     );
   }
